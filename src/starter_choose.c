f4f668e86df72ebb6fba9cfd425bb10e8cac1f73
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2022-11-21 04:42:09 +0900

    expose sStarterMon symbol

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -115,7 +115,7 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
     {8, 4},
 };
 
-static const u16 sStarterMon[STARTER_MON_COUNT] =
+const u16 sStarterMon[STARTER_MON_COUNT] =
 {
     SPECIES_TREECKO,
     SPECIES_TORCHIC,
